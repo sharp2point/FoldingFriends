a4e1edf97bd9c1772a8e674d222eef93cc8b8f59
Evgeny Blinov <sharppoint@yandex.ru>
2023-02-24 01:01:03 +0300

    переопределение роутинга и способа добавления компонентов друзей

--- public/components/user/user.js
+++ public/components/user/user.js
@@ -7,9 +7,13 @@ export class User extends HTMLElement {
   connectedCallback() {
     this.innerHTML = Template.render();
     this.dom = Template.map(this);
+    this.id = this.getAttribute("user-id");
+    this.name = this.getAttribute("user-name")
     this.dom.photo.src = this.getAttribute("user-photo");
-    this.dom.name.innerHTML = this.getAttribute("user-name");
+    this.dom.name.innerHTML = this.name;
   }
+  getId = ()=> this.id;
+  getName = ()=> this.name;
 }
 
 if (!customElements.get("vk-user")) {
