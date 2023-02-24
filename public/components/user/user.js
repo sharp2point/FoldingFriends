b2e79d46c52f8865f6af472f8bf4f73846922a00
Evgeny Blinov <sharppoint@yandex.ru>
2023-02-24 13:38:30 +0300

    code refactoring

--- public/components/user/user.js
+++ public/components/user/user.js
@@ -1,16 +1,25 @@
 import Template from "./template.js";
 
 export class User extends HTMLElement {
-  constructor(id,name,photo){
+  constructor(id, name, photo) {
     super();
-    this.attachShadow({mode: 'open'});
+    this.attachShadow({ mode: "open" });
     this.id = id;
     this.name = name;
     this.photo = photo;
-    this.shadowRoot.innerHTML = Template.render({name:this.name,photo:this.photo});
+    this.shadowRoot.innerHTML = Template.render({
+      name: this.name,
+      photo: this.photo,
+    });
+    this.dom = Template.map(this);
   }
-  getId = ()=> this.id;
-  getName = ()=> this.name;
+  setState = (state) => {
+    state
+      ? (this.dom.button.style.transform = "rotate(180deg)")
+      : (this.dom.button.style.transform = "rotate(0deg)");
+  };
+  getId = () => this.id;
+  getName = () => this.name;
 }
 
 if (!customElements.get("vk-user")) {
